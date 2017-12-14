b24ac18377db659e7d8c5c4e645a3a902cbb2c4a
Yuri Dirickson <ydirickson@gmail.com>
2017-12-14 08:43:37 -0200

    CONTACT FORM

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Course
+from .forms import ContactCourseForm
 
 def courses(request):
     context = {
@@ -9,7 +10,15 @@ def courses(request):
     return render(request,"courses/courses.html", context)
 
 def detail(request, slug):
-    context = {
-        "course":get_object_or_404(Course,slug=slug)
-    }
+    context = {}
+    if request.POST:
+        form = ContactCourseForm(request.POST)
+        if form.is_valid():
+            form.send_email()
+            context["sent"] = True
+            form = ContactCourseForm()
+    else:
+        form = ContactCourseForm()
+    context["course"] = get_object_or_404(Course,slug=slug)
+    context["form"] = form
     return render(request,"courses/course.html", context)
